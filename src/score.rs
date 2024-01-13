93c7facb299603ec6c6f7ba138e877a6a924b8be
Bluspur <craig-home@hotmai.com>
2024-01-13 18:09:52 +0000

    Add scoreboard setup in world space and introduced custom font

--- src/score.rs
+++ src/score.rs
@@ -1,11 +1,11 @@
 use bevy::prelude::*;
 
-use crate::{schedule::InGameSet, wall::GoalEvent, Side};
+use crate::{schedule::InGameSet, wall::GoalEvent, Side, HEIGHT};
 
 // Scoreboard
-const SCOREBOARD_FONT_SIZE: f32 = 42.;
+const SCOREBOARD_FONT_SIZE: f32 = 72.;
 const SCORE_COLOR: Color = Color::GOLD;
-const SCORE_GAP: Val = Val::Px(20.0);
+const SCORE_GAP: f32 = 20.;
 
 #[derive(Resource)]
 pub struct Score {
@@ -23,7 +23,7 @@ pub struct ScorePlugin;
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Score { left: 0, right: 0 })
-            .add_systems(Startup, setup_scoreboard)
+            .add_systems(Startup, setup_scoreboard_worldspace)
             .add_systems(
                 FixedUpdate,
                 (update_scoreboard, update_scores)
@@ -33,42 +33,40 @@ impl Plugin for ScorePlugin {
     }
 }
 
-fn setup_scoreboard(mut commands: Commands) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                width: Val::Percent(100.0),
-                height: Val::Auto,
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                column_gap: SCORE_GAP,
-                ..default()
-            },
+// Equivalent to the old way, but in world space so scores can be behind the ball
+fn setup_scoreboard_worldspace(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/PixelifySans-VariableFont_wght.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: SCOREBOARD_FONT_SIZE,
+        color: SCORE_COLOR,
+    };
+    commands.spawn((
+        Text2dBundle {
+            transform: Transform::from_translation(Vec3 {
+                x: -SCORE_GAP / 2.,
+                y: HEIGHT / 2.,
+                z: 0.,
+            }),
+            text: Text::from_section("0", text_style.clone()),
+            text_anchor: bevy::sprite::Anchor::TopRight,
             ..default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn(TextBundle::from_sections([TextSection::new(
-                    "0",
-                    TextStyle {
-                        font_size: SCOREBOARD_FONT_SIZE,
-                        color: SCORE_COLOR,
-                        ..default()
-                    },
-                )]))
-                .insert(ScoreText { side: Side::Left });
-            parent
-                .spawn(TextBundle::from_sections([TextSection::new(
-                    "0",
-                    TextStyle {
-                        font_size: SCOREBOARD_FONT_SIZE,
-                        color: SCORE_COLOR,
-                        ..default()
-                    },
-                )]))
-                .insert(ScoreText { side: Side::Right });
-        });
+        },
+        ScoreText { side: Side::Left },
+    ));
+    commands.spawn((
+        Text2dBundle {
+            transform: Transform::from_translation(Vec3 {
+                x: SCORE_GAP / 2.,
+                y: HEIGHT / 2.,
+                z: 0.,
+            }),
+            text: Text::from_section("0", text_style.clone()),
+            text_anchor: bevy::sprite::Anchor::TopLeft,
+            ..default()
+        },
+        ScoreText { side: Side::Right },
+    ));
 }
 
 fn update_scoreboard(scoreboard: Res<Score>, mut query: Query<(&mut Text, &ScoreText)>) {
